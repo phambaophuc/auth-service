5bc61bb1a23d6f8ce11a6f726a2221e4a6ac74bc
Pham Bao Phuc <123phuc27602@gmail.com>
2024-03-27 20:01:31 +0700

    first commit

---
+++ src/main/java/com/pbp/authservice/dto/response/JwtResponse.java
@@ -0,0 +1,19 @@
+package com.pbp.authservice.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class JwtResponse {
+
+    private String type = "Bearer";
+    private String accessToken;
+    private String refreshToken;
+
+    public JwtResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
